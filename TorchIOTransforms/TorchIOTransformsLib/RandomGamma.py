aca9deced88c1cdc3af9e06e851fcb171ac7f48b
Fernando <fepegar@gmail.com>
2021-08-31 18:07:02 +0100

    Fix argument name

--- TorchIOTransforms/TorchIOTransformsLib/RandomGamma.py
+++ TorchIOTransforms/TorchIOTransformsLib/RandomGamma.py
@@ -5,7 +5,7 @@ class RandomGamma(Transform):
     def setup(self):
         logs = self.getDefaultValue('log_gamma')
         self.logGammaSlider = self.makeRangeWidget(-2, *logs, 2, 0.01, 'log_gamma')
-        self.layout.addRow('Standard deviation: ', self.logGammaSlider)
+        self.layout.addRow('Log of gamma: ', self.logGammaSlider)
 
     def getKwargs(self):
         kwargs = dict(
