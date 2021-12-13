3697098c71931fbe6fa2d80354aeac8d3476b1cd
Fernando Pérez-García <fepegar@gmail.com>
2021-12-13 10:27:57 +0000

    Add rescale transform

---
+++ TorchIOTransforms/TorchIOTransformsLib/RescaleIntensity.py
@@ -0,0 +1,26 @@
+import qt
+
+from .Transform import Transform
+
+
+class RescaleIntensity(Transform):
+    def setup(self):
+        outMinMax = self.getDefaultValue('out_min_max')
+        self.outMinMaxSlider = self.makeRangeWidget(-1, *outMinMax, 1, 0.01, 'out_min_max')
+        self.layout.addRow('outMinMax: ', self.outMinMaxSlider)
+
+        percentiles = self.getDefaultValue('percentiles')
+        self.percentilesSlider = self.makeRangeWidget(0, *percentiles, 100, 0.01, 'percentiles')
+        self.layout.addRow('percentiles: ', self.percentilesSlider)
+
+        # inMinMax = self.getDefaultValue('in_min_max')
+        # self.inMinMaxSlider = self.makeRangeWidget(-1, *inMinMax, 1, 0.01, 'in_min_max')
+        # self.layout.addRow('inMinMax: ', self.inMinMaxSlider)
+
+    def getKwargs(self):
+        kwargs = dict(
+            out_min_max=self.getSliderRange(self.outMinMaxSlider),
+            percentiles=self.getSliderRange(self.percentilesSlider),
+            # in_min_max=self.getSliderRange(self.inMinMaxSlider),
+        )
+        return kwargs
--- TorchIOTransforms/TorchIOTransformsLib/__init__.py
+++ TorchIOTransforms/TorchIOTransformsLib/__init__.py
@@ -7,4 +7,21 @@ from .RandomGhosting import RandomGhosting
 from .RandomBiasField import RandomBiasField
 from .RandomAnisotropy import RandomAnisotropy
 from .RandomElasticDeformation import RandomElasticDeformation
+
+from .RescaleIntensity import RescaleIntensity
 from .HistogramStandardization import HistogramStandardization
+
+
+__all__ = (
+    RandomBlur,
+    RandomGamma,
+    RandomSpike,
+    RandomAffine,
+    RandomMotion,
+    RandomGhosting,
+    RandomBiasField,
+    RandomAnisotropy,
+    RandomElasticDeformation,
+    RescaleIntensity,
+    HistogramStandardization,
+)
