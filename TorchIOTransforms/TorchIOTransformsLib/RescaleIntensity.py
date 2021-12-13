72705905944a6ff977f400fb80f3fbf483d9a303
Fernando Pérez-García <fepegar@gmail.com>
2021-12-13 14:42:19 +0000

    Fix names

--- TorchIOTransforms/TorchIOTransformsLib/RescaleIntensity.py
+++ TorchIOTransforms/TorchIOTransformsLib/RescaleIntensity.py
@@ -7,11 +7,11 @@ class RescaleIntensity(Transform):
     def setup(self):
         outMinMax = self.getDefaultValue('out_min_max')
         self.outMinMaxSlider = self.makeRangeWidget(-1, *outMinMax, 1, 0.01, 'out_min_max')
-        self.layout.addRow('outMinMax: ', self.outMinMaxSlider)
+        self.layout.addRow('Output range: ', self.outMinMaxSlider)
 
         percentiles = self.getDefaultValue('percentiles')
         self.percentilesSlider = self.makeRangeWidget(0, *percentiles, 100, 0.01, 'percentiles')
-        self.layout.addRow('percentiles: ', self.percentilesSlider)
+        self.layout.addRow('Cut-off percentiles: ', self.percentilesSlider)
 
         # inMinMax = self.getDefaultValue('in_min_max')
         # self.inMinMaxSlider = self.makeRangeWidget(-1, *inMinMax, 1, 0.01, 'in_min_max')
